95932fce24f389b5a5c5c53c0ffd65b46a1eb753
Uria <dorman010@gmail.com>
2023-04-21 12:04:04 +0100

    use object constructors

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-const book1 ={title: "The Hobbit", 
-author: "J.R.R. Tolkien",
-pages:295, 
-read: false}
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author;
+    this.pages = pages
+    this.read = read
+
+}
+
+// const book1 ={title: "The Hobbit", 
+// author: "J.R.R. Tolkien",
+// pages:295, 
+// read: false}
 
 
 function bookInfo(book) {
 
 
-    if (book.read ==false) {
+    if (book.read == false) {
         return `${book.title} by ${book.author}, ${book.pages} pages, not read yet`
     }
     else {
@@ -17,4 +25,9 @@ function bookInfo(book) {
 
 
 
+const book1 = new Book ('The Hobbit', 'J.R.R. Tolkien', 295, true)
+const book2 = new Book ('Catcher in the Rye', 'J.D. Salinger','234', false)
+const book3 = new Book ('A Memory of Light', 'Robert Jordan', 909, true)
 console.log(bookInfo(book1))
+console.log(bookInfo(book2))
+console.log(bookInfo(book3))
